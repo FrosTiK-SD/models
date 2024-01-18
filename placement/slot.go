f8150a8a5be97503ce103582842e155a6bc2dfdf
Soumik Dutta <shalearkane@gmail.com>
2024-01-19 02:11:29 +0530

    update Slot struct

--- placement/slot.go
+++ placement/slot.go
@@ -3,12 +3,12 @@ package placement
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Slot struct {
-	Day                 primitive.DateTime
-	StartTime           primitive.DateTime
-	EndTime             primitive.DateTime
-	Name                string
-	Number              int // slot number
-	MaxInterviews       int // MaximumNumber of Interviews
-	PreferenceStartTime primitive.DateTime
-	PreferenceEndTime   primitive.DateTime
+	Day                 primitive.DateTime `json:"day,omitempty"`
+	StartDate           primitive.DateTime `json:"start_date,omitempty"`
+	EndDate             primitive.DateTime `json:"end_date,omitempty"`
+	Name                string             `json:"name,omitempty"`
+	Number              int                `json:"number,omitempty"`         // slot number
+	MaxInterviews       int                `json:"max_interviews,omitempty"` // MaximumNumber of Interviews
+	PreferenceStartTime primitive.DateTime `json:"preference_start_time,omitempty"`
+	PreferenceEndTime   primitive.DateTime `json:"preference_end_time,omitempty"`
 }
