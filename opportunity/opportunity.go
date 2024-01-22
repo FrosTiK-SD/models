d7d69976c2c6ab216b5221047521d8438d6b4730
Soumik Dutta <shalearkane@gmail.com>
2024-01-23 03:05:50 +0530

    add type in opportunity

--- opportunity/opportunity.go
+++ opportunity/opportunity.go
@@ -5,12 +5,20 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type OpportunityType string
+
+const (
+	MULTIPLE_PROFILE_SINGLE_WILLINGNESS   OpportunityType = "MULTIPLE_PROFILE_SINGLE_WILLINGNESS"
+	MULTIPLE_PROFILE_MULTIPLE_WILLINGNESS OpportunityType = "MULTIPLE_PROFILE_MULTIPLE_WILLINGNESS"
+)
+
 type Opportunity struct {
 	ID                     primitive.ObjectID     `bson:"id" json:"id,omitempty"`
 	Company                primitive.ObjectID     `bson:"company" json:"company,omitempty"`
 	Profiles               []primitive.ObjectID   `bson:"profile" json:"profile,omitempty"`
 	DetailsRequestedSchema map[string]interface{} `bson:"detailsRequestedSchema" json:"detailsRequestedSchema"`
 	Attachments            []misc.Attachment      `bson:"attachments" json:"attachments,omitempty"`
+	Type                   OpportunityType        `bson:"type" json:"type,omitempty"`
 
 	// metadata
 	CreatedAt primitive.DateTime `bson:"createdAt" json:"createdAt"`
