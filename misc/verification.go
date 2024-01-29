f4f269ec089cc9a1337c58c9ae6d9e074cda3de6
Soumik Dutta <shalearkane@gmail.com>
2024-01-30 03:34:26 +0530

    fix verification struct

--- misc/verification.go
+++ misc/verification.go
@@ -3,7 +3,7 @@ package misc
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Verification struct {
-	IsVerified bool               `bson:"isVerified" json:"isVerified,omitempty"`
-	VerifiedBy primitive.ObjectID `bson:"verifiedBy" json:"verifiedBy,omitempty"`
-	VerifiedAt primitive.DateTime `bson:"verifiedAt" json:"verifiedAt,omitempty"`
+	IsVerified bool               `bson:"isVerified" json:"isVerified"`
+	VerifiedBy primitive.ObjectID `bson:"verifiedBy" json:"verifiedBy"`
+	VerifiedAt primitive.DateTime `bson:"verifiedAt" json:"verifiedAt"`
 }
