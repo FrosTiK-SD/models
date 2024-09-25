6509a1741a647430afc9ca167135b869beba9aa4
Dev Raj R <dev.raj.r1729@gmail.com>
2024-09-25 23:37:20 +0530

    partial update of resume model

--- resume/resume.go
+++ resume/resume.go
@@ -3,7 +3,14 @@ package resume
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Resume struct {
-	ID      primitive.ObjectID
-	Student primitive.ObjectID
-	Title   string
+	ID         primitive.ObjectID `json:"_id" bson:"_id"`
+	Student    primitive.ObjectID `json:"student" bson:"student" binding:"required"`
+	Name       string             `json:"name" bson:"name" binding:"required"`
+	PDF        ResumePdf          `json:"pdf" bson:"pdf" binding:"required"`
+	IsVerified bool               `json:"isVerified" bson:"isVerified" binding:"required"`
+}
+
+type ResumePdf struct {
+	Url   string `json:"url,omitempty" bson:"url,omitempty"`
+	Latex string `json:"latex,omitempty" bson:"latex,omitempty"`
 }
